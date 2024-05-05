150e0189370a1c6f12469874cf45e3b9e7ce71ab
Jeff Jiang <jeff.jiang13@gmail.com>
2024-05-05 02:18:47 -0400

    working on stripe

--- src/actions/manage.subscription.ts
+++ src/actions/manage.subscription.ts
@@ -1,6 +1,10 @@
-"use server";
 import { connectDb } from "@/shared/libs/db";
+import Membership from "@/models/membership.model";
 import Stripe from "stripe";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+  apiVersion: "2023-10-16",
+});
 
 export const manageSubscription = async ({
   customerId,
@@ -9,9 +13,27 @@ export const manageSubscription = async ({
 }) => {
   try {
     await connectDb();
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-      apiVersion: "2023-10-16",
-    });
+
+    // Properly type the response to include the subscriptions expansion
+    const customer = await stripe.customers.retrieve(customerId, {
+      expand: ['subscriptions'],
+    }) as Stripe.Customer & { subscriptions: Stripe.ApiList<Stripe.Subscription> };
+
+    const latestSubscription = customer.subscriptions.data[0]; // Now safe to access
+
+    const newPlanId = latestSubscription.items.data[0].price.product;
+
+    // Retrieve the product linked to the plan
+    const product = await stripe.products.retrieve(newPlanId as string);
+
+    const membership = await Membership.findOne({ stripeCustomerId: customerId });
+    if (membership) {
+      // Assume product.name contains the plan name
+      await Membership.updateOne(
+        { stripeCustomerId: customerId },
+        { $set: { plan: product.name } }
+      );
+    }
 
     const portalSession = await stripe.billingPortal.sessions.create({
       customer: customerId,
@@ -20,6 +42,7 @@ export const manageSubscription = async ({
 
     return portalSession.url;
   } catch (error) {
-    console.log(error);
+    console.error("Error in manageSubscription:", error);
+    throw error;
   }
 };
