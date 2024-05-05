c441286d29184aba591d0c7883f1b873ae0c9280
Jeff Jiang <jeff.jiang13@gmail.com>
2024-05-05 00:36:32 -0400

    mobile friendly

--- src/shared/widgets/dashboard/sidebar/user.plan.tsx
+++ src/shared/widgets/dashboard/sidebar/user.plan.tsx
@@ -17,7 +17,6 @@ const UserPlan = () => {
       history.push(res);
     });
   };
-  console.log('member',membershipData)
   return (
     <div className="w-full my-3 p-3 bg-[#FDF1F8] rounded hover:shadow-xl cursor-pointer">
       <div className="w-full flex items-center">
